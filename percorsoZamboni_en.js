144d35e715035c774e9cacba58d0cca89edce385
Cécile Picard-Limpens <ccl@ccl-laptop.local>
2017-04-05 09:18:38 +0200

    languages EN/IT; adding video in modal windows / sound stops when widows are shut

--- percorsoZamboni_en.js
+++ percorsoZamboni_en.js
@@ -15,7 +15,7 @@ var features = [];
 
 function initMap() {
 
-    $.getJSON( "features.json", function( data ) {
+    $.getJSON( "features_en.json", function( data ) {
         //console.log(data);
         for (var i=0; i< data.length; i++) {
             features.push(data[i]);
@@ -37,7 +37,7 @@ function initMap() {
                 '<div class="modal-content">' +
                 '<div class="modal-header">' +
                 '<button type="button" class="close" data-dismiss="modal">&times;</button>' +
-                '<h4 class="modal-title">Tappa ' + i + '</h4>' +
+                '<h4 class="modal-title">Stage ' + i + '</h4>' +
                 '</div>' +
                 '<div class="modal-body">' +
                 '<iframe id="videoContent" width="100%" height="100%" src="https://www.youtube.com/embed/em5PRRO-sK0" frameborder="0" allowfullscreen></iframe>' + //video could a feature!!
@@ -56,7 +56,7 @@ function initMap() {
             '<div class="modal-content">' +
             '<div class="modal-header">' +
             '<button type="button" class="close" data-dismiss="modal">&times;</button>' +
-            '<h4 class="modal-title">Zamboni Touch Street: Un percorso in 7 Tappe</h4>' +
+            '<h4 class="modal-title">Zamboni Touch Street: A path in 7 Stages</h4>' +
             '</div>' +
             '<div class="modal-body">' +
             '<iframe id="videoContent" width="100%" height="100%" src="https://www.youtube.com/embed/6ju8xO_Zvfo" frameborder="0" allowfullscreen></iframe>' + //video could a feature!!
@@ -67,7 +67,8 @@ function initMap() {
             '</div>' +
             '</div>' +
             '</div>')
-        // myModal opens as introduction 
+
+        // myModal opens as introduction     
         $('#myModal').modal('show');
         var url = $('#myModal iframe').attr('src');
         /* Assign empty url value to the iframe src attribute when
